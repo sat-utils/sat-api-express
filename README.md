3fa5f939b6340a70ace332e083d56e91f18f8793
Scisco <scisco7@gmail.com>
2016-05-09 17:41:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## sat-api-express
+
+This is an express implementation of sat-api.
+
+### Installation
+
+Make sure you have an instance of Elasticsearch running. To populate it with Landsat and Sentinel metadata refer to [landsat8-metadata](https://github.com/sat-utils/landsat8-metadata) and [sentinel2-metadata](https://github.com/sat-utils/sentinel2-metadata)
+
+    $ npm install
+    $ node index.js
+
---
+++ index.js
@@ -0,0 +1,64 @@
+require("envloader").load();
+var express = require('express');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var api = require('sat-api-lib');
+var app = express();
+
+var search = function (action, req, res) {
+  var s = new api(req);
+  s[action](function (err, resp) {
+    if (err) {
+      res.status(400).send({details: err});
+    }
+    res.send(resp);
+  });
+};
+
+/*----------------------------------
+// START MIDDLEWARES
+----------------------------------*/
+app.use(compression())
+app.use(bodyParser.json({ type: 'application/json' })); // for parsing application/json
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  if (err.status === 400 && err.name === 'SyntaxError' && err.body) {
+    res.status(err.status).send({details: 'Invalid JSON'})
+  }
+
+  res.status(err.status).send({details: err.body.slice(0, 100).toString()});
+});
+/*----------------------------------
+// END MIDDLEWARES
+----------------------------------*/
+
+/*----------------------------------
+// START ENDPOINTS
+----------------------------------*/
+app.get('/', function(req, res) {
+  search('simple', req, res);
+});
+
+app.post('/', function (req, res) {
+  search('simple', {query: req.body}, res);
+});
+
+app.get('/count', function(req, res) {
+  search('count', req, res);
+});
+
+app.get('/geojson', function(req, res) {
+  search('geojson', req, res);
+});
+
+app.post('/geojson', function(req, res) {
+  search('geojson', {query: req.body}, res);
+});
+/*----------------------------------
+// END ENDPOINTS
+----------------------------------*/
+
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
