7932e624d9910e1471a95e74d136e03dfe686df4
Devseed-CI <dev@developmentseed.org>
2017-08-08 15:32:11 +0000

    CI deploy [skip ci]

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Sat API",
+  "url": "api.developmentseed.org/satellites",
+  "name": "sat-api-express",
+  "version": "1.0.0",
+  "description": "An express implementation of sat-api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-08-08T15:32:11.165Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
